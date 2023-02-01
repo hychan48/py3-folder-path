daa2a59daf0826b383d03ee396c38ea431e96ac3
jasonchan <jchan48@gmail.com>
2023-01-31 19:34:30 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# py3-folder-path
+https://github.com/hychan48/py3-folder-path
+* Learning how to use Python import paths
+* $PYTHONPATH
+* sites
+  * .pth
---
+++ tests/alias.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        from a.b.b1 import hello_world
+        out = hello_world()
+        self.assertEqual(out, "Hello World")  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
